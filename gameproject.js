6615968e4fab3d820f41d33554a599c480ac5bc1
22130mb <22130mb@hvhs.school.nz>
2025-03-11 00:30:17 +0000

    added movemtn

--- gameproject.js
+++ gameproject.js
@@ -4,11 +4,13 @@
 
 
 // Variables 
-var Ballnumber=1;
+var timerValue = 10;
+var Ballnumber = 1;
+
 // Setup 
 function setup() {
     console.log("setup: ");
-    cnv = new Canvas(1000, 1000); 
+    cnv = new Canvas(1000, 1000);
 
     bat = new Sprite(400, 400, 10, 10, 'd');
     bat.color = 'white';
@@ -26,16 +28,23 @@ function setup() {
     for (let i = 0; i < Ballnumber; i++) {
         let Ball = new Sprite(random(100, 900), random(100, 900), 50, 50, 'd');
         BallGroup.add(Ball);
+        Ball.vel.x = random(2, 5);
+        Ball.vel.y = random(2, 5);
         // code for "balls" making them appear randomly
-    }
 
+    }
 }
 // Draw
 function draw() {
-	background('black');
-    bat.moveTowards(mouseX, mouseY, 1);
-}
-if (mouse.presses()) {
-// this code makes the shape move towards where the mouse is 
-shape.moveTo(xPos, yPos, 100);
+    background('black');
+
+    if (kb.pressing('w')) {
+        Ball.vel.y = 5;
+    } else if (kb.pressing('s')) {
+        Ball.vel.y = -5;
+    } else if (kb.pressing('a')) {
+        Ball.vel.x = 5;
+    } else if (kb.pressing('d')) {
+        Ball.vel.x = -5;
+    }
 }
