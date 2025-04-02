0d68e2b9b8f7a2308d22d6b60f9b30244ad317e1
22130mb <22130mb@hvhs.school.nz>
2025-04-02 20:54:56 +0000

    fixed hitbox

--- gameproject.js
+++ gameproject.js
@@ -2,15 +2,15 @@
 // Written by Max Bergman
 
 
-var timerValue = 10; 
+var timerValue = 10;
 var Ballnumber = 1; // Number of balls on screen
-var Score = 0; 
-var timer = 10; 
+var Score = 0;
+var timer = 10;
 var currentBall; // current ball in play
-var imgFace, imgHammer; 
-var Background; 
+var imgFace, imgHammer;
+var Background;
 var gameState = "start"; // decides whether the game is set to start, playing or end 
-var restartButton; // restarts game variable
+var restartButton; // restarts game 
 function preload() {
     // Load all the required images before the game starts
     imgBG = loadImage('images/pixil-frame-0.png');
@@ -20,14 +20,15 @@ function preload() {
 }
 
 function setup() {
-  
-    createCanvas(1000, 1000); 
-    imgFace.resize(150, 150); 
+
+    createCanvas(1000, 1000);
+    imgFace.resize(150, 150);
 
     // Create the hammer (bat) sprite, initially hidden
-    bat = new Sprite(400, 400, 10, 10, 'd');
+    bat = new Sprite(400, 400, 400, 400, 'd');
     bat.img = imgHammer;
-    bat.scale = 0.1; 
+    bat.scale = 0.2;
+
     bat.visible = false; // Makes bat invisible until game start
 
     // Create wall sprites to keep sprites on screen
@@ -36,21 +37,21 @@ function setup() {
     wallTop = new Sprite(500, 0, 1000, 15, 'k');
     wallBot = new Sprite(500, 1000, 1000, 15, 'k');
 
- 
+
     wallLH.color = 'white';
     wallRH.color = 'white';
     wallTop.color = 'white';
     wallBot.color = 'white';
 
-   //makes walls invisible until game start
+    //makes walls invisible until game start
     wallLH.visible = false;
     wallRH.visible = false;
     wallTop.visible = false;
     wallBot.visible = false;
 
-    // Create restart button, initially hidden
+
     restartButton = createButton("Restart");
-    restartButton.position(width / 2 - 50, height / 2 + 75); 
+    restartButton.position(width / 2 - 50, height / 2 + 75);
     restartButton.mousePressed(restartGame); // restarts game
     restartButton.hide(); // hides button
 }
@@ -69,7 +70,7 @@ function draw() {
         wallTop.visible = true;
         wallBot.visible = true;
 
-       
+
         bat.position.x = mouseX;
         bat.position.y = mouseY;
 
@@ -77,7 +78,7 @@ function draw() {
         displayTimer();
     } else if (gameState === "end") {
         showEndScreen(); // Show the end screen when the game is over
-    } 
+    }
 }
 
 function mousePressed() {
@@ -90,7 +91,7 @@ function mousePressed() {
 
 function displayTimer() {
     textSize(100);
-    fill('red');
+    fill('Black');
     textAlign(CENTER, CENTER);
     text(timer, width / 2, height / 2); //  timer in the center 
 
@@ -106,7 +107,7 @@ function displayTimer() {
 function score() {
     // show the player's score on screen
     textSize(35);
-    fill('red');
+    fill('Black');
     text("Score: " + Score, 80, 50); // 
 }
 
@@ -117,8 +118,8 @@ function createBall() {
 
     // Create a new ball at a random position
     currentBall = new Sprite(random(100, 900), random(100, 900), 50, 50, 'k');
-    currentBall.img = imgFace; // the ball's image
-    currentBall.scale = 0.5; // the ball size
+    currentBall.img = imgFace;
+    currentBall.scale = 0.5;
 
     //  collision between ball and bat
     currentBall.collides(bat, function (ball, bat) {
@@ -134,7 +135,7 @@ function showStartScreen() {
     textSize(50);
     fill('white');
     textAlign(CENTER, CENTER);
-    text("Click to Start", width / 2, height / 2); 
+    text("Click to Start", width / 2, height / 2);
     restartButton.hide(); // hides restart button
 }
 
@@ -144,18 +145,18 @@ function showEndScreen() {
     fill('white');
     textAlign(CENTER, CENTER);
     text("Game Over!", width / 2, height / 2); // game over text and line below shows score
-    text("Score: " + Score, width / 2, height / 2 + 50); 
+    text("Score: " + Score, width / 2, height / 2 + 50);
     restartButton.show(); // show restart button after timer finishes
-
+    
     // Hide sprites when the game is over
     bat.visible = false;
     wallLH.visible = false;
     wallRH.visible = false;
     wallTop.visible = false;
     wallBot.visible = false;
-    
+
     if (currentBall) {
-        currentBall.remove(); 
+        currentBall.remove();
     }
 }
 
